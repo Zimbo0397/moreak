42488f1dfa32bd7f67730f9ded3e19c0770e6d66
Zimbo0397 <zimbo0397@Gmail.com>
2016-11-11 18:09:16 +0200

    + foto , video

--- src/js/main.js
+++ src/js/main.js
@@ -28,3 +28,34 @@ $(window).on('load', function() {
 		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 	});
 })
+
+
+// Blueimp Init
+// document.getElementById('links').onclick = function (event) {
+//     event = event || window.event;
+//     var target = event.target || event.srcElement,
+//         link = target.src ? target.parentNode : target,
+//         options = {index: link, event: event},
+//         links = this.getElementsByTagName('a');
+//     blueimp.Gallery(links, options);
+// };
+
+
+$(window).on('load', function() {
+	$('.iframe-holder').each(function() {
+
+		$(this).on('click', function() {
+
+			$self= $(this),
+			selfData = $self.attr('data-href');
+
+			$('.iframe-holder').removeClass('active');
+			$self.addClass('active');
+			$self.html('<iframe src="'+selfData+'" frameborder="0" allowfullscreen></iframe>');
+			console.log($self)
+			
+		})
+
+	});
+	
+});
